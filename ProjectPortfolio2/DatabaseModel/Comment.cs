4c9d1463ed6e964614cf819a6be45e9b50f207d8
Nicolai <nicolaielhoej@hotmail.com>
2018-10-25 10:44:33 +0200

    added comment and commentmarked db models

---
+++ ProjectPortfolio2/DatabaseModel/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectPortfolio2
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int? Score { get; set; }
+        public string Text { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public int PostId { get; set; }
+        public int OwnerId { get; set; }
+    }
+
+}
---
+++ ProjectPortfolio2/DatabaseModel/CommentMarked.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ProjectPortfolio2
+{
+    public class CommentMarked
+    {
+        public int CommentId { get; set; }
+        public int UserId { get; set; }
+        public string AnnotationText { get; set; }
+    }
+}
