b4839b618e25b5bcbe7063dffefcf96ce9016cc3
Nicolai <nicolaielhoej@hotmail.com>
2018-10-25 11:05:44 +0200

    added some dbmodels

--- ProjectPortfolio2/DatabaseModel/Comment.cs
+++ ProjectPortfolio2/DatabaseModel/Comment.cs
@@ -5,7 +5,7 @@ namespace ProjectPortfolio2
     public class Comment
     {
         public int Id { get; set; }
-        public int? Score { get; set; }
+        public int? Score { get; set; } // ? means that the value can be null - not needed on strings
         public string Text { get; set; }
         public DateTime? CreationDate { get; set; }
         public int PostId { get; set; }
---
+++ ProjectPortfolio2/DatabaseModel/PostLink.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProjectPortfolio2.DatabaseModel
+{
+    public class PostLink
+    {
+       public int PostId { get; set; }
+        public int LinkId { get; set; }
+    }
+}
