37b27130915c458ce2174102857b877b8451aa3b
Filip Germanek <filipgermanek@yahoo.com>
2018-10-25 10:45:44 +0200

    merge conflict resolved

---
+++ ProjectPortfolio2/DatabaseModel/Tag.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProjectPortfolio2.DatabaseModel
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ProjectPortfolio2/DatabaseModel/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ProjectPortfolio2.DatabaseModel
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
