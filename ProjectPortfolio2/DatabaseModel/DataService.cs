804d2293765c885ca1abcb35557a36bcf040a841
Filip Germanek <filipgermanek@yahoo.com>
2018-10-26 15:03:57 +0200

    webservice getowners example

---
+++ ProjectPortfolio2/DatabaseModel/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectPortfolio2.DatabaseModel
+{
+    public interface IDataService
+    {
+        List<Owner> GetOwners();
+    }
+    public class DataService : IDataService
+    {
+        public List<Owner> GetOwners()
+        {
+            using (var db = new DatabaseContext())
+            {
+                //TODO this is just a testing query
+                return db.Owners.Take(5).ToList();
+            }
+        }
+    }
+}
