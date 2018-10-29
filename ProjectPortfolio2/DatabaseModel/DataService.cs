a5db5992486d8eda8fd932d7758db6d3243ad9b6
Filip Germanek <filipgermanek@yahoo.com>
2018-10-29 10:44:32 +0100

    usermodel, userlistmode, usercontroller, user datservice functions

--- ProjectPortfolio2/DatabaseModel/DataService.cs
+++ ProjectPortfolio2/DatabaseModel/DataService.cs
@@ -8,6 +8,9 @@ namespace ProjectPortfolio2.DatabaseModel
     {
         List<Owner> GetOwners();
         Owner GetOwner(int id);
+        List<User> GetUsers();
+        User GetUser(int id);
+        List<SearchHistory> GetUserSearchHistory(int userId);
     }
     public class DataService : IDataService
     {
@@ -25,6 +28,30 @@ namespace ProjectPortfolio2.DatabaseModel
             using (var db = new DatabaseContext())
             {
                 return db.Owners.Find(id);
+            }
+        }
+
+        public List<User> GetUsers()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Users.ToList();
+            }
+        }
+
+        public User GetUser(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Users.Find(id);
+            }
+        }
+
+        public List<SearchHistory> GetUserSearchHistory(int userId)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.SearchHistories.Where(s => s.UserId.Equals(userId)).ToList();
             }
         }
     }
