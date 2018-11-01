c187dca37e0f4bddc154d18be42f8a9de73f150c
Filip Germanek <filipgermanek@yahoo.com>
2018-11-01 11:09:47 +0100

    merge fixes

--- WebService/Models/CommentListModel.cs
+++ WebService/Models/CommentListModel.cs
@@ -9,11 +9,7 @@ namespace WebService.Models
     public class CommentListModel
     {
         public String Url { get; set; }
-        //public int Id { get; set; }
         public int? Score { get; set; } // ? means that the value can be null - not needed on strings
         public string Text { get; set; }
-        //public DateTime? CreationDate { get; set; }
-        //public int PostId { get; set; }
-        //public int OwnerId { get; set; }
     }
 }
--- WebService/Models/CommentModel.cs
+++ WebService/Models/CommentModel.cs
@@ -8,11 +8,8 @@ namespace WebService.Models
 {
     public class CommentModel
     {
-        //public int Id { get; set; }
         public int? Score { get; set; } // ? means that the value can be null - not needed on strings
         public string Text { get; set; }
         public DateTime? CreationDate { get; set; }
-        //public int PostId { get; set; }
-        //public int OwnerId { get; set; }
     }
 }
